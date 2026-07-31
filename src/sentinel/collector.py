c6446c7eb02edee545eaa7bc672a0dee097e3988
Sebastian Kubacki <seb.kubacki@gmail.com>
2026-07-31 20:52:49 +0200

    feat: collect basic system information

--- src/sentinel/collector.py
+++ src/sentinel/collector.py
@@ -1,8 +1,19 @@
 import subprocess
 
-system_info = {}
+def get_command_output(command):
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        return output.decode('utf-8').strip()
+    except subprocess.CalledProcessError as e:
+        return f"Error executing command '{' '.join(command)}': {e.output.decode('utf-8').strip()}"
 
 def collect_system_info():
-    hostname = subprocess.check_output(['hostname']).decode('utf-8').strip()
-    system_info['hostname'] = hostname
+    system_info = {}
+    system_info['Hostname'] = get_command_output(['hostname'])
+    system_info['Kernel version'] = get_command_output(['uname', '-r'])
+    system_info['Uptime'] = get_command_output(['uptime', '-p'])
+    system_info['Disk usage'] = get_command_output(['df', '-h', '/'])
+    system_info['Memory'] = get_command_output(['free', '-h'])
+    system_info['IP Address'] = get_command_output(['hostname', '-I'])
+    
     return system_info
--- src/sentinel/output.py
+++ src/sentinel/output.py
@@ -1,7 +1,9 @@
 from collector import collect_system_info
 
 def show_system_info():
-    print("Collecting system information...")
+    print('============================')
+    print("Sentinel System Report")
+    print('============================')
     info = collect_system_info()
     for key, value in info.items():
-        print(f"{key}: {value}")
+        print(f"{key}: {value}")  
